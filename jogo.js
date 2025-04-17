0e2d1e5a7d0bbbe89629e1db602875ea6c63a9a9
biadjordjevic <biadjordjevic@gmail.com>
2025-04-16 23:11:23 -0300

    subindo alterações

--- jogo.js
+++ jogo.js
@@ -10,33 +10,34 @@ function gerarSequencia(){
     const tamanho = Math.floor(Math.random() * (tamanhoMax - tamanhoMin + 1)) + tamanhoMin;
     const numeros = [];
     for(let i = 0; i < tamanho; i++){
-        numeros.push(Math.floor(Math.random() * 10))
+        numeros.push(Math.floor(Math.random() * 10));
     }
-    return numeros
+    return numeros;
 }
 
+// Monta o teclado
 const teclado = document.getElementById("teclado");
 for (let i = 0; i <= 9; i++) {
     const btn = document.createElement("button");
     btn.textContent = i;
-    btn.onclick = adicionarNumero(i);
+    btn.onclick = () => adicionarNumero(i);
     teclado.appendChild(btn);
 }
 
-function adicionarNumero(){
-    if (tentativa.lenght < sequencia.length){
-        tentativa.push(numero)
-        atualizarTentaiva();
+function adicionarNumero(numero){
+    if (tentativa.length < sequencia.length){
+        tentativa.push(numero);
+        atualizarTentativa();
     }
 }
 
-function atualizarTentaiva(){
-    document.getElementById("palpite".textContent = "Sua tentativa: " + tentativa.join (""));
+function atualizarTentativa(){
+    document.getElementById("palpite").textContent = "Sua tentativa: " + tentativa.join("");
 }
 
 function limparTentativa(){
     tentativa = [];
-    atualizarTentaiva();
+    atualizarTentativa();
     document.getElementById("mensagem").textContent = "";
 }
 
@@ -44,36 +45,65 @@ function enviarTentativa(){
     if (tentativa.length !== sequencia.length){
         document.getElementById("mensagem").textContent = "Sua tentativa não está completa!";
         return;
- }
-    const acertou = tentativa.every((num, i) => num === sequencia[i]);
-    
-    if(acertou){
-        document.getElementById("mensagem").textContent = "Você acertou a sequência!";
     }
-    else{
+
+    const acertou = tentativa.every((num, i) => num === sequencia[i]);
+
+    if(acertou){
+        let pontosGanhos = 0;
+
+        if (dica1 && dica2) {
+            pontosGanhos = 5;
+        } else if (dica1 || dica2) {
+            pontosGanhos = 15;
+        } else {
+            pontosGanhos = 20;
+        }
+
+        pontuacao += pontosGanhos;
+
+        document.getElementById("mensagem").textContent =
+        `Você acertou a sequência! (+${pontosGanhos} pontos)`;
+
+        document.getElementById("pontuacao").textContent = `Pontuação: ${pontuacao}`;
+
+        if (pontuacao >= 30) {
+        alert("🎉 Parabéns! Você venceu o jogo!");
+        pontuacao = 0;
+        document.getElementById("pontuacao").textContent = `Pontuação: ${pontuacao}`;
+        }
+
+        sequencia = gerarSequencia();
+        dica1 = false;
+        dica2 = false;
+        tentativa = [];
+        atualizarTentativa();
+        document.getElementById("dica2").disabled = true;
+
+    } else {
         document.getElementById("mensagem").textContent = "Tente novamente";
     }
 }
 
 function mostrarDica(){
-    const soma = sequencia.reduce((acc, val) => acc = val, 0);
+    const soma = sequencia.reduce((acc, val) => acc + val, 0);
     document.getElementById("mensagem").textContent = "Dica: a soma dos números da sequência é: " + soma;
-    dica1 = true
-    document.getElementById(dica2).disaled = false;
+    dica1 = true;
+    document.getElementById("dica2").disabled = false;
 }
 
 function mostrarDica2(){
-    const primeironumero = sequencia[0];
-    document.getElementById("mensagem").textContent = "Dica 2: o primeiro número da sequência é " + primeironumero
+    const primeiroNumero = sequencia[0];
+    document.getElementById("mensagem").textContent = "Dica 2: o primeiro número da sequência é " + primeiroNumero;
     dica2 = true;
 }
 
 function revelarResposta(){
     if (dica1 && dica2){
-        document.getElementById("mensagem").textContent = "A sequêcia era: " + sequencia.join(""); 
-    }
-    else {
-        document.getElementById("mensagem").textContent = "Você precisa usar a duas dicas para saber a resposta"
+        document.getElementById("mensagem").textContent = "A sequência era: " + sequencia.join(""); 
+    } else {
+        document.getElementById("mensagem").textContent = "Você precisa usar as duas dicas para ver a resposta.";
     }
 }
 
+let pontuacao = 0;
